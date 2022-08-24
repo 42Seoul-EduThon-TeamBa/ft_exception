5812c89eaeef7a61e224543e074b5c1c99ca9cfa
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-08-24 15:52:04 +0900

    feat: ex00 test script + file

---
+++ tester/ex00/test_ex00.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Build
+gcc -Wall -Wextra -Werror -o test_generated ex00.c test_ex00.c
+if [ $? -ne 0 ]
+then
+    echo "Compilation failed"
+    exit 1
+fi
+
+# Run
+
+function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
+
+function test_case {
+    echo "Test $1"
+    timeout 5s ./test_generated $3 > /dev/null
+    if [ $? -ne $2 ]
+    then
+        echo "Test $1 KO :("
+        exit 1
+    fi
+    echo "Test $1 OK :D"
+}
+
+# case 1 - no call register fpe
+test_case 1 136
+
+# case 2 - call register fpe
+test_case 2 42 "catch"
