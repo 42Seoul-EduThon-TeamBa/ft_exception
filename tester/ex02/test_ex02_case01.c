ba9cddb5e46cabc277d423ae84302d110c985aa7
Elec <elec@Elecui-MacBookPro.local>
2022-08-24 15:57:19 +0900

    01, 02

---
+++ tester/ex02/test_ex02_case01.c
@@ -0,0 +1,27 @@
+#include <signal.h>
+
+extern int arg;
+extern int res;
+
+int get_exit_status(int k)
+{
+    if (!res)
+        // try로 끝나지 않음.
+        return 1;
+    if (k != arg)
+        // try_function으로 끝나지 않음.
+        return 1;
+
+    return 0;
+}
+
+int try_function(void)
+{
+    res = 1;
+    return arg;
+}
+
+void catch_function(int sig)
+{
+    res = 0;
+}
